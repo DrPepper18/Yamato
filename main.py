c94b853e84379f3c3215ade945cef61cc881e1be
DrPepper <122201748+DrPepper18@users.noreply.github.com>
2024-07-18 23:34:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,53 +5,29 @@ token = open('token.txt').readline()
 bot = telebot.TeleBot(token)
 
 startup = (
-    'Привет, на связи Димон, он же Димас, он же Димыч, он же ДИМОООООНТУРУРУРУРУРУ.\n\n'
-    'Увлекаюсь вебом и data science. Всегда рад отправиться в неизведанные мне места и попробовать что-то новое.'
-    'Потихоньку вот играю на гитаре, в последнее время читаю и смотрю всякое про саморазвитие.\n\n'
-    'Про что этот канал? Сложно сказать. Люблю сюда выложить свои мысли, мемчики, красивые фото и просто поделиться'
-    'похождениями и рандомными интересными моментами из повседневности.\n\n'
-    'Предупреждаю, возможен передоз кринжом. Но если вам нужен теплый уютный уголок, то welcome. '
-    'По крайней мере, я таким постараюсь его сделать.\n\n'
-    '/update')
-roadmap = (
-    'Роудмап:\n'
-    '1. Сделать ссылки внутри текста (DONE!)\n'
-    '2. Автоматизировать добавление и группировку постов (хэштеги/NLP)\n'
-    '3. Премиум-эмодзи внутри бота\n'
-    '4. Сделать удобным для использования в ЛЮБОМ телеграм-канале.\n')
+    'Here is your startup message'
+    '')
 links = {
-    'Мероприятия': [
-        ['МТС True Tech Champ', 'https://t.me/bluscreendez/246'],
-        ['DANO', 'https://t.me/bluscreendez/354'],
-        ['ОММО', 'https://t.me/bluscreendez/444'],
-        ['НТО АБП. 2023-2024', 'https://t.me/bluscreendez/480'],
-    ],
-    'Куда занесло': [
-        ['Яндекс (главный офис)', 'https://t.me/bluscreendez/69'],
-        ['Яндекс (Москва-Сити)', 'https://t.me/bluscreendez/140'],
-        ['Тинькофф (Водный стадион)', 'https://t.me/bluscreendez/81'],
-        ['Т-Space', 'https://t.me/bluscreendez/464'],
-        ['VK', 'https://t.me/bluscreendez/431'],
-        ['МИСиС', 'https://t.me/bluscreendez/538'],
-        ['МТС', 'https://t.me/bluscreendez/666'],
-    ],
-    'Мемасы': []
+    'Key': [
+        ['Post name', 'Post link'],
+        ['Post name 2', 'Post link 2'],
+    ]
 }
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, roadmap, parse_mode='html')
+    bot.send_message(message.chat.id, startup, parse_mode='html')
 
 
 @bot.message_handler(commands=['update'])
 def button_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Скинь всё, что есть")
+    item1 = types.KeyboardButton("All")
     markup.add(item1)
     for key in links.keys():
         markup.add(types.KeyboardButton(key))
-    bot.send_message(message.chat.id, 'Группы постов обновлены, выбирайте!', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Updated, check it!', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
@@ -62,7 +38,7 @@ def message_reply(message):
             linklist += f'<a href="{link[1]}">{link[0]}</a>\n'
         return linklist
 
-    if message.text == "Скинь всё, что есть":
+    if message.text == "All":
         for key in links.keys():
             bot.send_message(message.chat.id, get_linklist(key), parse_mode='html')
     else:
